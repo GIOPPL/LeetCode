4335a6e83c1e7e97997e55081a3a0567fc118055
GIOPPL <1766468447@qq.com>
2021-07-18 10:15:32 +0800

    215

---
+++ src/Solution215.java
@@ -0,0 +1,48 @@
+public class Solution215 {
+    public static void main(String[] args) {
+        long startTime = System.nanoTime(); //获取开始时间
+        start();
+        long endTime = System.nanoTime(); //获取结束时间
+        System.out.print(("程序运行时间：" + (endTime - startTime) / 1000 + "k ns"));
+    }
+
+    private static void start() {
+        Solution215 s = new Solution215();
+        int[] n1 = new int[]{3, 2, 3, 1, 2, 4, 5, 5, 6};
+        int[] n2 = new int[]{3, 2, 1, 5, 6, 4};
+        int[] n3 = new int[]{1};
+        int a = s.findKthLargest(n3, 1);
+        System.out.println(a);
+    }
+
+    public int findKthLargest(int[] nums, int k) {
+        if (k <= 0 || k > nums.length)
+            return -1;
+        return sort(nums, 0, nums.length - 1, k);
+    }
+
+    private int sort(int[] nums, int l, int r, int k) {
+        int x = nums[l];
+        int i = l;
+        int j = r;
+        while (i < j) {
+            while (i < j && nums[j] > x) j--;
+            if (i < j) {
+                nums[i++] = nums[j];
+            }
+            while (i < j && nums[i] < x) i++;
+            if (i < j) {
+                nums[j--] = nums[i];
+            }
+        }
+        nums[i] = x;
+        int n = nums.length - i;
+        if (n > k) {
+            return sort(nums, i + 1, r, k);
+        } else if (n < k) {
+            return sort(nums, l, i - 1, k);
+        } else {
+            return nums[i];
+        }
+    }
+}
